f1ac284707de7ef6e5f4e6dd1f8a1c546bb60982
brinaningrum <sabrina030206@gmail.com>
2026-06-05 11:20:51 +0700

    second commit

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
-#program simple calculator
-operator = str(input('Pilih operator "penambahan", "pengurangan", "perkalian", atau "pembagian": '))
-number1 = int(input("Masukkan angka pertama: "))
-number2 = int(input("Masukkan angka kedua: "))
+#program untuk cek angka ganjil atau genap secara acak
+n1 = int(input("Masukkan angka pertama: "))
+n2 = int(input("Masukkan angka kedua: "))
 
 
-if operator == 'penambahan':
-  res = number1 + number2
-elif operator == 'pengurangan':
-  res = number1 - number2
-elif operator == 'perkalian':
-  res = number1 * number2
-elif operator == 'pembagian':
-  res = number1 / number2
-print(f'{operator} dari {number1} dan {number2} adalah {res}')
+for i in range(n1,n2+1):
+  if i % 2 == 0:
+    res = 'Genap'
+  elif i % 2 != 0:
+    res = 'Ganjil'
+  print(i,res)
